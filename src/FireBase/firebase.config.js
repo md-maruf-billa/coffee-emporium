1dca9c692e80f3d2158bf862f64547c7ac0e1785
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-04-24 16:09:20 +0600

    Remove api kye

--- src/FireBase/firebase.config.js
+++ src/FireBase/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDLLDh6wnD79_1qcfkGHrJKSUTytTn-Vm4",
-    authDomain: "coffees-emporium.firebaseapp.com",
-    projectId: "coffees-emporium",
-    storageBucket: "coffees-emporium.appspot.com",
-    messagingSenderId: "289425934887",
-    appId: "1:289425934887:web:2ba5fd67fbce8dc0899c02"
+    apiKey:process.env.apiKey,
+    authDomain:process.env.authDomain,
+    projectId:process.env.projectId,
+    storageBucket:process.env.storageBucket,
+    messagingSenderId:process.env.messagingSenderId,
+    appId:process.env.appId,
 };
 
 // Initialize Firebase
